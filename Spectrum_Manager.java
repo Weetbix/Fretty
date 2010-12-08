0ad3130023da9341137035f351934eb6186e84f1
John Hannagan <john.hannagan@gmail.com>
2010-12-08 11:28:19 -0800

    Outputs the mean of the ROIs in the current ROI manager and stack of images as a table.
    This is about to change to the average.

--- Spectrum_Manager.java
+++ Spectrum_Manager.java
@@ -5,11 +5,52 @@ import java.awt.*;
 import java.awt.event.*;
 import ij.plugin.frame.*;
 
+import ij.measure.*;
+
+//Generates the mean of all the currently selected ROIs
+//Outputs them as a table, for all slices
 class MeanGenerator implements ActionListener
 {
+	private RoiManager roi;
+	private ImagePlus imp;
+	private ResultsTable resultsWindow;
+
+	public boolean ROIManagerCheck()
+	{
+		roi = RoiManager.getInstance();
+		if( roi == null )
+		{
+			IJ.showMessage( "Error", "You must have the ROI manager open and some region selected" );
+			return false;
+		}
+		return true;
+	}
+
 	public void actionPerformed( ActionEvent e )
 	{
-		IJ.showMessage("Clicked!");	
+		//Requires ROI manager
+		if( !ROIManagerCheck() ) return;
+		imp = IJ.getImage();
+		resultsWindow = new ResultsTable();
+		resultsWindow.reset(); 
+
+
+		ImageStatistics ims;
+		for( int i = 1; i <= imp.getStackSize(); i++ )
+		{
+			resultsWindow.incrementCounter();
+
+			for( int roi_num = 0; roi_num < roi.getCount(); roi_num++ )
+			{
+				roi.select( roi_num );
+				imp.setSliceWithoutUpdate( i );
+				ims = imp.getStatistics( Measurements.MEAN );
+
+				resultsWindow.addValue( "Mean (" + Integer.toString(roi_num) + ")", ims.mean );
+			}
+		}
+
+		resultsWindow.show( "Herbert" );
 	}
 }
 
