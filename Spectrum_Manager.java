544cc5a303e667e651094d669387840b8b3bcbf0
John Hannagan <john.hannagan@gmail.com>
2010-12-07 17:30:19 -0800

    Started creating the spectrum manager GUI Added plugin Spectrum_Manager Currently only opens the ROI manager...

---
+++ Spectrum_Manager.java
@@ -0,0 +1,52 @@
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import java.awt.event.*;
+import ij.plugin.frame.*;
+
+class Mean_Generator implements ActionListener
+{
+	public void actionPerformed( ActionEvent e )
+	{
+		IJ.showMessage("Clicked!");	
+	}
+}
+
+public class Spectrum_Manager extends PlugInFrame {
+
+	public Spectrum_Manager () 
+	{
+		super("Fretty Spectrum Manager");
+
+		setLayout( new FlowLayout() );
+
+		/////////////////////////////////////////////////////////
+		//Setup gui and callbacks
+		////////////////////////////////////////////////////////
+
+		Button ROIOpener = new Button( "Open ROI Manager" );
+		ROIOpener.addActionListener( 
+			new ActionListener(){ 
+				public void actionPerformed( ActionEvent e ) {
+					if( RoiManager.getInstance() == null ) 
+					{
+						IJ.run( "ROI Manager..." );
+					}
+					else
+					{
+						RoiManager.getInstance().setVisible( true );
+					}
+				}
+			} );
+		add( ROIOpener );
+
+		Button meanGenerator = new Button( "Calculate Spectrum" );
+		meanGenerator.addActionListener( new Mean_Generator() );
+		add( meanGenerator );
+
+		pack();
+		GUI.center(this);
+		setVisible( true );
+	}
+}
