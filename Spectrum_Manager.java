57350480787c7d1c994dffb6c3b8ca5d9d6d41a0
John Hannagan <john.hannagan@gmail.com>
2010-12-08 11:53:05 -0800

    SpectrumGenerator class finished (I think). Now outputs the total of the means of the ROIs for each slice in the stack

--- Spectrum_Manager.java
+++ Spectrum_Manager.java
@@ -7,9 +7,10 @@ import ij.plugin.frame.*;
 
 import ij.measure.*;
 
-//Generates the mean of all the currently selected ROIs
-//Outputs them as a table, for all slices
-class MeanGenerator implements ActionListener
+//Generates a spectrum from the current image stack. The spectrum is
+//calculated as the total of all the mean values of the ROIs for the current slice.
+//Outputs them as a table, for all slices.
+class SpectrumGenerator implements ActionListener
 {
 	private RoiManager roi;
 	private ImagePlus imp;
@@ -18,7 +19,7 @@ class MeanGenerator implements ActionListener
 	public boolean ROIManagerCheck()
 	{
 		roi = RoiManager.getInstance();
-		if( roi == null )
+		if( roi == null || roi.getCount() <= 0 ) 
 		{
 			IJ.showMessage( "Error", "You must have the ROI manager open and some region selected" );
 			return false;
@@ -34,23 +35,27 @@ class MeanGenerator implements ActionListener
 		resultsWindow = new ResultsTable();
 		resultsWindow.reset(); 
 
-
-		ImageStatistics ims;
+		ImageStatistics stats;
 		for( int i = 1; i <= imp.getStackSize(); i++ )
 		{
-			resultsWindow.incrementCounter();
-
+			double total = 0;
 			for( int roi_num = 0; roi_num < roi.getCount(); roi_num++ )
 			{
+				//For each ROI in the current slice, find the mean value
 				roi.select( roi_num );
 				imp.setSliceWithoutUpdate( i );
-				ims = imp.getStatistics( Measurements.MEAN );
 
-				resultsWindow.addValue( "Mean (" + Integer.toString(roi_num) + ")", ims.mean );
+				//Maybe slow? Seems to calculate the other stats even though we 
+				//only asked it to calculate the mean. 
+				stats = imp.getStatistics( Measurements.MEAN );
+				total += stats.mean;
 			}
+
+			resultsWindow.incrementCounter();
+			resultsWindow.addValue( "Total Emission Wavelength", total );
 		}
 
-		resultsWindow.show( "Herbert" );
+		resultsWindow.show( "Spectrum" );
 	}
 }
 
@@ -83,7 +88,7 @@ public class Spectrum_Manager extends PlugInFrame {
 		add( ROIOpener );
 
 		Button meanGenerator = new Button( "Calculate Spectrum" );
-		meanGenerator.addActionListener( new MeanGenerator() );
+		meanGenerator.addActionListener( new SpectrumGenerator() );
 		add( meanGenerator );
 
 		pack();
