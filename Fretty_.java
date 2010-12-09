a66d956383b82d9d10b20a1dbd16b1e41554467f
John Hannagan <john.hannagan@gmail.com>
2010-12-09 10:55:47 -0800

    Still building base GUI, added more controls

--- Fretty_.java
+++ Fretty_.java
@@ -11,11 +11,42 @@ import javax.swing.border.*;
 import javax.*;
 import javax.swing.plaf.*;
 
+//The panel at the top of the gui screen...
+class FrettyTopPanel extends JPanel
+{
+	//Padding
+	private static final Insets insets = new Insets( 10,10,10,10 );
+	public Insets getInsets() { return insets; }
+
+	public FrettyTopPanel()
+	{
+		setLayout( new GridLayout( 4, 2 ) );
+		add( new JLabel( "Cross Excitation Correction" ) );
+		JCheckBox crossCorrection = new JCheckBox();
+		add( crossCorrection );
+
+		add( new JLabel("Wavelengths Per Sample  " ) );
+		JSpinner wavelengthsPerSample = new JSpinner(
+					new SpinnerNumberModel( 25, 1, 9999, 1 ) );
+		add( wavelengthsPerSample );
+
+		add( new JLabel( "Donor quantum yield" ) );
+		JSpinner donorQuantumYield = new JSpinner( 
+					new SpinnerNumberModel( (double)150, 0, 40000, 0.0001 ) );
+		add( donorQuantumYield );
+
+		add( new JLabel( "Acceptor quantum yield" ) );
+		JSpinner acceptorQuantumYield = new JSpinner( 
+					new SpinnerNumberModel( (double)150, 0, 40000, 0.0001 ) );
+		add( acceptorQuantumYield );
+	}
+}
+
 class FrettyCommonPanel extends JPanel 
 {
 	public FrettyCommonPanel()
 	{
-		setBorder( new TitledBorder("Common") );
+		setBorder( new TitledBorder("Common Tools") );
 		add( new Button("Open ROI Manager") );
 		add( new Button("Background Reduction" ) );
 	}
@@ -80,9 +111,9 @@ class FrettyFRETSamplesPanel extends JPanel
 	}
 }
 
-public class Fretty extends PlugInFrame 
+public class Fretty_ extends PlugInFrame 
 {
-	public Fretty () 
+	public Fretty_ () 
 	{
 		super("Fretty");
 
@@ -99,11 +130,11 @@ public class Fretty extends PlugInFrame
 		}
 		catch(Exception e ){};
 
-		JPanel p = new FrettyCommonPanel();
-		add(p);
-
+		add( new FrettyTopPanel() );
+		add( new FrettyCommonPanel() );
 		add( new FrettyReferenceSpectraPanel() );
 		add( new FrettyFRETSamplesPanel() );
+
 		pack();
 		GUI.center(this);
 		setVisible( true );
