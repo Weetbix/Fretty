b863e86a07b1b185357e5d4b5a7109e34aed4972
John Hannagan <john.hannagan@gmail.com>
2010-12-13 11:53:16 -0800

    wrote setters/getters for fret processor

--- FRETProcessor.java
+++ FRETProcessor.java
@@ -6,11 +6,144 @@ import ij.*;
 //////////////////////////////////////////////////////////////////////////
 public class FRETProcessor 
 {
-	
-	
-	FRETProcessor()
+	boolean crossExcitationCorrection = true;
+	int wavelengthsPerSample = 0;
+	float donorQuantumYield = 0;
+	float acceptorQuantumYield = 0;
+
+	//Reference Spectra
+	Spectrum SDD;
+	Spectrum SAD;
+	Spectrum SAA;
+
+	ImagePlus donorExcitationStack;
+	ImagePlus acceptorExcitationStack;
+
+	public FRETProcessor()
 	{
 		
 	}
 
+	/////////////////////////////////////////////////
+	//Boring setters and getters
+	/////////////////////////////////////////////////
+
+	void enableCrossExcitationCorrection( boolean yesno ) 
+	{
+		crossExcitationCorrection = yesno;
+	}
+
+	void setWavelengthsPerSample( int wlps )
+	{
+		wavelengthsPerSample = wlps;
+	}
+
+	int getWavelengthsPerSample()
+	{
+		return wavelengthsPerSample;
+	}
+
+	boolean setDonorQuantumYield( float val ) 
+	{
+		if( val < 0 || val >1 ) return false;
+		donorQuantumYield = val;
+		return true;
+	}
+
+	float getDonorQuantumYield()
+	{
+		return donorQuantumYield;
+	}
+
+	boolean setAcceptorQuantumYield( float val )
+	{
+		if( val < 0 || val > 1 ) return false;
+		acceptorQuantumYield = val;
+		return true;
+	}
+
+	float getAcceptorQuantumYield() 
+	{
+		return acceptorQuantumYield;
+	}
+
+	void setSDDSpectrum( Spectrum s )
+	{
+		SDD = s;
+	}
+	
+	Spectrum getSDDSpectrum()
+	{
+		return SDD;
+	}
+
+	void setSADSpectrum( Spectrum s ) 
+	{
+		SAD = s;
+	}
+
+	Spectrum getSADSpectrum()
+	{
+		return SAD;
+	}
+
+	void setSAASpectrum( Spectrum s ) 
+	{
+		SAA = s;
+	}
+
+	Spectrum getSAASpectrum()
+	{
+		return SAA;
+	}
+
+	public void CreateFRETImage()
+	{
+		
+	}
+
+	void setDonorExcitationStack( ImagePlus stack )
+	{
+		donorExcitationStack = stack;
+	}
+
+	ImagePlus getDonorExcitationStack()
+	{
+		return donorExcitationStack;
+	}
+
+	void setAcceptorExcitationStack( ImagePlus stack )
+	{
+		acceptorExcitationStack = stack;
+	}
+
+	ImagePlus getAcceptorExcitationStack()
+	{
+		return acceptorExcitationStack;
+	}
+
+
+
+
+
+
+	//Checks that all params are setup correctly to call CreateFRETImage, throws 
+	//approrpiate exceptions if something is wrong, with the message set.
+	private void imageErrorChecks()
+	{
+		if( crossExcitationCorrection )
+		{
+			if( acceptorExcitationStack == null ) 
+				throw new NullPointerException( "No acceptor excitation stack indicated" );
+
+			if( SAA == null )
+				throw new NullPointerException( "No SAA spectrum loaded" );
+		}
+
+		//Think I can do this by throwing in the setters....instead..
+		/*if( donorQuantumYield < 0 || donorQuantumYield > 1 ) 
+			throw new illegalArgumentException( "Donor quantum yield should be between 0 and 1 (inclusive)" );
+
+		if( acceptorQuantumYield*/	
+	}
 }
