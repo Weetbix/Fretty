636ac75e803d10bb9d71600f5948bd84f8fb5e93
John Hannagan <weetbix_@hotmail.com>
2010-12-22 11:57:29 -0800

    Made batching panel more pretty by balancing the widgets on left and right so it is now smaller aswell

--- BatchingPanel.java
+++ BatchingPanel.java
@@ -7,15 +7,17 @@ public class BatchingPanel extends JPanel
 {
 	public BatchingPanel()
 	{
-		add( new JButton("Build Mega Stack") );
+		JPanel leftPanel = new JPanel();
+		leftPanel.setLayout( new GridLayout( 2, 1 ) );
+		leftPanel.add( new JButton("Build Mega Stack") );
+		leftPanel.add( new JCheckBox("Include megastacks") );
 
 		JPanel wildcardPanel = new JPanel();
-			wildcardPanel.setLayout( new GridLayout(2,1) );
 			wildcardPanel.setBorder( new TitledBorder("Stack names (wildcard)") );
-		
+			wildcardPanel.setLayout( new GridLayout(1,1) );
 			wildcardPanel.add( new JTextField(15) );
-			wildcardPanel.add( new JCheckBox("Include megastacks") );
 
+		add( leftPanel );
 		add( wildcardPanel );
 	}
 }
